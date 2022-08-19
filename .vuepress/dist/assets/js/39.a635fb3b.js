d053c5be4e091be21be64b9b1836a42ded6317de
DatPT <dat.pham1@ntq-solution.com.vn>
2022-08-20 02:24:46 +0700

    change

---
+++ .vuepress/dist/assets/js/39.a635fb3b.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[39],{304:function(t,s,e){"use strict";e.r(s);var n=e(13),r=Object(n.a)({},(function(){var t=this._self._c;return t("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[t("meta",{attrs:{"http-equiv":"refresh",content:"0;url=/1.0/sites/the-basics.html"}})])}),[],!1,null,null,null);s.default=r.exports}}]);
